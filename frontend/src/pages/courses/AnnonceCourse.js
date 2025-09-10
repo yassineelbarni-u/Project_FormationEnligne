12a481d65c7043aec9df753e9db5554c64b8b805
Yassine El Barni <yassine.elbarni-etu@etu.univh2c.ma>
2025-09-10 20:36:33 +0100

    Add style page cours gratuis (Animation , Coleur principale)

--- frontend/src/pages/courses/AnnonceCourse.js
+++ frontend/src/pages/courses/AnnonceCourse.js
@@ -38,10 +38,6 @@ const AnnonceCourse = () => {
 
       <main className="main-content">
         <div className="container">
-          {/* <div className="page-header">
-            <h1>Nos Annonces de Cours</h1>
-            <p>Découvrez toutes nos formations disponibles</p>
-          </div> */}
 
           {loading ? (
             <div className="loading-state">
